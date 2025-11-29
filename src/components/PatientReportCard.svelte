8e1a28b01eff6684d487d5e1e03dc333026fda6e
ParzivalPavlis <lacinapavel04@gmail.com>
2025-11-29 01:46:08 +0100

    ✨ feat: implement patient and report management with dynamic rendering

---
+++ src/components/PatientReportCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { PatientReportsRecord } from '$types/pocketbase';
+  import * as Card from '$components/ui/card/index.js';
+  import { page } from '$app/state';
+  let { patientReport }: { patientReport: PatientReportsRecord } = $props();
+  let currentPath = page.url.pathname;
+</script>
+
+<a href="{currentPath}/report/{patientReport.id}">
+  <Card.Root class="cursor-pointer gap-2 transition-shadow">
+    <Card.Header>
+      <Card.Title>Report 1 for Patient {patientReport.patient}</Card.Title>
+    </Card.Header>
+    <Card.Content></Card.Content>
+  </Card.Root>
+</a>
---
+++ src/routes/(restricted)/patients/[patientId]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { page } from '$app/state';
+  import PatientReportCard from '$components/PatientReportCard.svelte';
+  import type { IsoAutoDateString, PatientReportsRecord } from '$types/pocketbase';
+  let patientId = page.params.patientId;
+  const patientReports: PatientReportsRecord[] = [
+    {
+      id: '1',
+      patient: '1',
+      record: ['1'],
+      report: [],
+      created: new Date().toISOString() as IsoAutoDateString,
+      updated: new Date().toISOString() as IsoAutoDateString
+    },
+    {
+      id: '2',
+      patient: '2',
+      record: ['2'],
+      report: [],
+      created: new Date().toISOString() as IsoAutoDateString,
+      updated: new Date().toISOString() as IsoAutoDateString
+    }
+  ];
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+  {#if patientId}
+    {#each patientReports as report (report.id)}
+      {#if report.patient === patientId}
+        <PatientReportCard patientReport={report} />
+      {/if}
+    {/each}
+  {/if}
+</div>
