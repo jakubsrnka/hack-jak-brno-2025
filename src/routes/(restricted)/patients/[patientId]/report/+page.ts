17a00642ba5d6f16a12da0e53decc94d9bcda075
Jakub Srnka <jakub.srnka@volny.cz>
2025-11-29 23:54:48 +0100

    🚸 redirect report without a slug

---
+++ src/routes/(restricted)/patients/[patientId]/report/+page.ts
@@ -0,0 +1,6 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async ({url}) => {
+  redirect(302, url.pathname.replace(/report\/?$/, ''));
+}
