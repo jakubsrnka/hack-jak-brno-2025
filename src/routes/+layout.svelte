922983066b7d121c4fa402ffab06e3d72e83d915
Martin Přívozník <prim14@vse.cz>
2025-11-29 01:32:53 +0100

    💩 chore: removed unused imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import { Toaster } from 'svelte-sonner';
   import favicon from '$lib/assets/favicon.svg';
-  import * as Sidebar from '$components/ui/sidebar/index.js';
-  import AppSidebar from '$components/AppSidebar.svelte';
   import UploadDocumentDialog from '$components/UploadDocumentDialog.svelte';
   import { uploadDocumentDialogOpen } from '$lib/stores/dialog';
   import '../app.css';
