77bc4c8c7c1c9495c86ba5c5f04d56a6c10957af
Jakub Srnka <jakub.srnka@volny.cz>
2025-11-29 03:26:01 +0100

    🐛 fix getPatientReport typing

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -1,9 +1,9 @@
 import { pbClient } from "$lib/pocketbase";
 import type { KeyPart } from "$types/openai";
-import { Collections, type PatientRecordsResponse } from "$types/pocketbase";
+import { Collections, type PatientRecordsResponse, type PatientReportsResponse } from "$types/pocketbase";
 
-export const getPatientReport = async (id: string): Promise<PatientRecordsResponse<{
-  "patientRecords_via_report": PatientRecordsResponse<KeyPart[]>
+export const getPatientReport = async (id: string): Promise<PatientReportsResponse<{
+  "patientRecords_via_report": PatientRecordsResponse<KeyPart[]>[]
 }>> => pbClient.collection(Collections.PatientReports).getOne(id, {
   expand: "patientRecords_via_report"
 })
