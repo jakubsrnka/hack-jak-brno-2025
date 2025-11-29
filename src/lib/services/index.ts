7a48c12c5b626198d092d0bf9a0b3c973234d36e
Jakub Srnka <jakub.srnka@volny.cz>
2025-11-29 03:27:59 +0100

    🎨 format services

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -1,9 +1,18 @@
-import { pbClient } from "$lib/pocketbase";
-import type { KeyPart } from "$types/openai";
-import { Collections, type PatientRecordsResponse, type PatientReportsResponse } from "$types/pocketbase";
+import { pbClient } from '$lib/pocketbase';
+import type { KeyPart } from '$types/openai';
+import {
+  Collections,
+  type PatientRecordsResponse,
+  type PatientReportsResponse
+} from '$types/pocketbase';
 
-export const getPatientReport = async (id: string): Promise<PatientReportsResponse<{
-  "patientRecords_via_report": PatientRecordsResponse<KeyPart[]>[]
-}>> => pbClient.collection(Collections.PatientReports).getOne(id, {
-  expand: "patientRecords_via_report"
-})
+export const getPatientReport = async (
+  id: string
+): Promise<
+  PatientReportsResponse<{
+    patientRecords_via_report: PatientRecordsResponse<KeyPart[]>[];
+  }>
+> =>
+  pbClient.collection(Collections.PatientReports).getOne(id, {
+    expand: 'patientRecords_via_report'
+  });
