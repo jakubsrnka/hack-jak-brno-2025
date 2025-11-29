5bb3acc4ca636eefd138f44642cd94638ad00c4d
Jakub Srnka <jakub.srnka@volny.cz>
2025-11-29 11:36:31 +0100

    🧑‍💻 improve sandbox usage

--- src/routes/(restricted)/sandbox/+page.svelte
+++ src/routes/(restricted)/sandbox/+page.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
   import { Button } from '$components/ui/button';
+  import { Skeleton } from '$components/ui/skeleton';
   import { getPatientReport } from '$lib/services';
 
   let buttonText = $state('Send to AI');
 
   let input = $state<string | null>(null);
   let output = $state<string | null>(null);
+  let loading = $state(false);
 
   const handleClick = async () => {
     try {
+      loading = true;
+      input = null;
+      output = null;
       buttonText = 'Retrieving...';
-      const report = await getPatientReport('xeot8g23swit03b');
+      const report = await getPatientReport('pspmpywfqmwk8v7');
       input = JSON.stringify(report, null, 2);
       buttonText = 'Sending...';
 
@@ -27,11 +32,17 @@
       console.log('AI Response:', data);
       output = JSON.stringify(data, null, 2);
       buttonText = 'Done';
+      loading = false;
       setTimeout(() => {
         buttonText = 'Send to AI';
       }, 2000);
     } catch (error) {
       console.error('Error fetching AI response:', error);
+      buttonText = 'Error! Try again';
+      loading = false;
+      setTimeout(() => {
+        buttonText = 'Send to AI';
+      }, 2000);
     }
   };
 </script>
@@ -40,12 +51,14 @@
   <h1 class="mb-4 text-2xl font-bold">AI Report Summary:</h1>
 
   <Button onclick={handleClick}>{buttonText}</Button>
-  {#if output}
-    <div class="max-w-full mt-4 overflow-x-auto">
+  <div class="max-w-full mt-4 overflow-x-auto">
+    {#if output}
       <h2 class="mt-6 mb-2 text-xl font-semibold">Input:</h2>
       <pre>{input}</pre>
       <h2 class="mt-6 mb-2 text-xl font-semibold">Output:</h2>
       <pre>{output}</pre>
-    </div>
-  {/if}
+    {:else if loading}
+      <Skeleton class="h-96 w-full mt-4" />
+    {/if}
+  </div>
 </div>
