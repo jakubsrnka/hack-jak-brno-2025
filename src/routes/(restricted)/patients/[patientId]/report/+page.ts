643ccf9bc2a1c2b616f372a292874e26c8c1002f
Jakub Srnka <jakub.srnka@volny.cz>
2025-11-30 01:14:19 +0100

    🎨 format code

--- src/routes/(restricted)/patients/[patientId]/report/+page.ts
+++ src/routes/(restricted)/patients/[patientId]/report/+page.ts
@@ -1,6 +1,6 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageLoad } from "./$types";
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({url}) => {
+export const load: PageLoad = async ({ url }) => {
   redirect(302, url.pathname.replace(/report\/?$/, ''));
-}
+};
